d2dd64bdcce6affa1afa5825ebc7ff6d7eebdd9a
Yogesh Parnandi <yogi.30995@gmail.com>
2017-08-11 23:16:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -26,6 +26,8 @@ int dxl_get_highbyte( int word );
 void dxl_write_word( int id, int address, int value );
 int dxl_hal_tx( unsigned char *pPacket, int numPacket );
 int dxl_hal_open( int devIndex, int baudrate );
+void USART1_Configuration(int baudrate);
+void TxDByte_DXL(unsigned char p);
 
 
 int dxl_initialize( int devIndex, int baudnum )
@@ -115,3 +117,37 @@ int dxl_hal_tx( unsigned char *pPacket, int numPacket )
 
 	return numPacket;
 }
+void USART1_Configuration(int baudrate){
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	
+	GPIO_InitTypeDef GPIO_Struc;
+	
+	GPIO_Struc.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_Struc.GPIO_Pin=GPIO_Pin_10;
+	GPIO_Struc.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_Struc);
+	
+	GPIO_Struc.GPIO_Mode=GPIO_Mode_IN_FLOATING;
+	GPIO_Struc.GPIO_Pin=GPIO_Pin_11;
+	GPIO_Init(GPIOB,&GPIO_Struc);
+	
+	USART_InitTypeDef USART_Struc;
+	USART_Struc.USART_BaudRate=baudrate;
+	USART_Struc.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
+	USART_Struc.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
+	USART_Struc.USART_Parity=USART_Parity_No;
+	USART_Struc.USART_StopBits=USART_StopBits_1;
+	USART_Struc.USART_WordLength=USART_WordLength_8b;
+	
+	USART_Cmd(USART1,ENABLE);
+	USART_HalfDuplexCmd(USART1,ENABLE);
+	
+}
+
+void TxDByte_DXL(unsigned char p){
+	USART_SendData(USART1,p);
+	 while(USART_GetFlagStatus(USART1,USART_FLAG_RXNE) == RESET);
+}
+
+
